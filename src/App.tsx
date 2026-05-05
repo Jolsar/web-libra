caa8c75903e3b9fb35efe62d2154966496d41b25
Jolsar <55783904+Jolsar@users.noreply.github.com>
2026-05-05 14:32:18 +0200

    Use official store badge URLs

--- src/App.tsx
+++ src/App.tsx
@@ -187,7 +187,10 @@ export default function App() {
               target="_blank"
               rel="noreferrer"
             >
-              <img src="/badges/app-store.svg" alt="Download on the App Store" />
+              <img
+                src="https://developer.apple.com/assets/elements/badges/download-on-the-app-store.svg"
+                alt="Download on the App Store"
+              />
             </a>
             <a
               className="store-badge"
@@ -195,7 +198,10 @@ export default function App() {
               target="_blank"
               rel="noreferrer"
             >
-              <img src="/badges/google-play.png" alt="Get it on Google Play" />
+              <img
+                src="https://play.google.com/intl/en_us/badges/static/images/badges/en_badge_web_generic.png"
+                alt="Get it on Google Play"
+              />
             </a>
           </div>
         </div>
